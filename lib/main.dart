b9b91ba456a492ae6df4b9df55ef094eb1a9451a
Rabyn-Collab <babynshrestha64@gmail.com>
2023-05-07 16:57:00 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,60 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutternew/provider/count_provider.dart';
 import 'package:flutternew/view/home_page.dart';
 import 'package:get/get.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main (){
+
+Future<String> getSome() async{
+  return Future.delayed(Duration(seconds: 2), (){
+    return 'hello world';
+  });
+}
+
+Future getNews() async{
+  final dio = Dio();
+  try{
+    final response = await dio.get('https://free-news.p.rapidapi.com/v1/search',
+    queryParameters: {
+      'q': 'hollywood',
+      'lang': 'en'
+    },
+      options: Options(
+        headers: {
+          'X-RapidAPI-Key': '89e53c72d7msh16aa8c041814a4cp1f3e79jsn333d7bcaf747',
+          'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
+        }
+      )
+    );
+    return response.data;
+  }on DioError  catch (err){
+    return err.response;
+  }
+}
+
+
+Future getTopRated() async{
+  final dio = Dio();
+  try{
+    final response = await dio.get('https://api.themoviedb.org/3/movie/top_rated',
+        queryParameters: {
+          'api_key': '2a0f926961d00c667e191a21c14461f8',
+          'page': 1
+        },
+    );
+    return response.data;
+  }on DioError  catch (err){
+    return err.response;
+  }
+}
+
+
+void main () async {
+ final data = await getNews();
+ final data1 = await getTopRated();
+ print(data1);
+   // getSome().then((value) => print(value)).catchError((err) => print(err));
 
 List person = [
   {'id': 1, 'title': 'hello'},
