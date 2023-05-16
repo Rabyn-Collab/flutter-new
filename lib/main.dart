12db518044e54301d2a7d7470c572ec777bdd405
Rabyn-Collab <babynshrestha64@gmail.com>
2023-05-16 16:20:01 +0545

    chnages

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'firebase_options.dart';
 
 
 
 
 void main () async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
  runApp(ProviderScope(child: Home()));
 
